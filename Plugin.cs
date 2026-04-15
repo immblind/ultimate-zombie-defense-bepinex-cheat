aac7937b5436f2e1c41180c238e24a4be01721cf
immblind <mariosoldier34@gmail.com>
2026-04-15 16:09:43 +0200

    see update v1.0.2 patchnotes

--- Plugin.cs
+++ Plugin.cs
@@ -10,6 +10,9 @@ using UnityEngine.Rendering;
 using Knife.Effects;
 using Knife.Effects.SimpleController;
 using UnityEngine.TextCore;
+using UnityEngine.PlayerLoop;
+using JetBrains.Annotations;
+using System.Reflection;
 
 namespace imcheat;
 
@@ -41,34 +44,41 @@ public class Plugin : BaseUnityPlugin
     public static ConfigEntry<float> Damage;
 
     public static ConfigEntry<bool> EnableForceHalloween;
+
+    public static ConfigEntry<bool> BuildAnytime;
     private void Awake()
     {
-        maxBuilds = Config.Bind("Host", "Max build limit", 99999999, "The maximum build limit (Applies to all buildings)");
-        EnableCustomBuildLimit = Config.Bind("Host", "Enable custom nuild limit", true, "Enable or not the custom build limit.");
+        //Host(or host and client have plugin) only features
+        maxBuilds = Config.Bind("------ Host ------", "Max build limit", 99999999, "The maximum build limit (Applies to all buildings)");
+        EnableCustomBuildLimit = Config.Bind("------ Host ------", "Enable custom build limit", true, "Enable or not the custom build limit.");
 
-        CustomCashReward = Config.Bind("Host", "Cash reward per wave", 9999999f, "Custom cash reward for every wave");
-        EnableCustomCashReward = Config.Bind("Host", "Enable custom cash reward", true, "Enable or not the custom cash reward value");
+        CustomCashReward = Config.Bind("------ Host ------", "Cash reward per wave", 9999999f, "Custom cash reward for every wave");
+        EnableCustomCashReward = Config.Bind("------ Host ------", "Enable custom cash reward", true, "Enable or not the custom cash reward value");
 
-        CustomWaveSkip = Config.Bind("Host", "Custom wave skip", 5, "Allows to skip a number of waves after each wave.");
-        EnableWaveSkip = Config.Bind("Host", "Enable custom wave skip", false, "Enables or not the custom wave skip value");
+        CustomWaveSkip = Config.Bind("------ Host ------", "Custom wave skip", 5, "Allows to skip a number of waves after each wave.");
+        EnableWaveSkip = Config.Bind("------ Host ------", "Enable custom wave skip", false, "Enables or not the custom wave skip value");
 
-        CustomWalkSpeed = Config.Bind("Client", "Custom walk speed", 8f, new ConfigDescription("You can go as fast as you want! Default is around 8.", new AcceptableValueRange<float>(0.01f, 100f)));
-        EnableCustomWalkSpeed = Config.Bind("Client", "Enable custom walk speed", false, "Enable or not your custom walk speed value");
+        NoBuildingBlock = Config.Bind("------ Host ------", "No building block", false, "Removes building limits.");
 
-        NoBuildingBlock = Config.Bind("Host", "No building block", false, "Removes building limits.");
+        EnableGodmode = Config.Bind("------ Host ------", "Godmode", false, "Enables or not godmode (Become invincile)");
 
-        EnableGodmode = Config.Bind("Host", "Godmode", false, "Enables or not godmode (Become invincile)");
+        InfiniteDamage = Config.Bind("------ Host ------", "Infinite damage", false, "One shot everything.");
 
-        InfiniteDamage = Config.Bind("Host", "Infinite damage", false, "One shot everything.");
+        EnableForceHalloween = Config.Bind("------ Host ------", "Force halloween", false, "Force the game to use exclusive halloween content");
 
-        EnableForceHalloween = Config.Bind("Host", "Force halloween", false, "Force the game to use exclusive halloween content");
+        //Client features(work both as host and as client even if host doesnt have the plugin)
+        CustomWalkSpeed = Config.Bind("------ Client ------", "Custom walk speed", 8f, new ConfigDescription("You can go as fast as you want! Default is around 8.", new AcceptableValueRange<float>(0.01f, 100f)));
+        EnableCustomWalkSpeed = Config.Bind("------ Client ------", "Enable custom walk speed", false, "Enable or not your custom walk speed value");
 
-        InfiniteAmmo = Config.Bind("Client", "Infinite ammo", true, "Shooting doesnt consume any ammo.");
+        InfiniteAmmo = Config.Bind("------ Client ------", "Infinite ammo", true, "Shooting doesnt consume any ammo.");
+
+        BuildAnytime = Config.Bind("------ Client ------", "Force build mode", true, "Build menu will always be usable, you can build, repair and sell builds at any time. Works in multiplayer.");
 
         Logger = base.Logger;
         var harmony = new Harmony("immblind.plugins.imcheat");
         harmony.PatchAll();
-        int modcount = 9;
+
+        int modcount = 10;
         Plugin.Logger.LogInfo($"Loaded imcheat with {modcount} mods.");
 
     }
@@ -106,7 +116,7 @@ class NexWavePatcher2                                                        //D
 
     static bool Prepare()
     {
-        return Plugin.EnableWaveSkip.Value;
+        return Plugin.EnableWaveSkip.Value && Plugin.CustomWaveSkip.Value>1;
     }
     static bool Prefix(ZS_WaveSystemServer __instance)
     {
@@ -125,11 +135,8 @@ class NexWavePatcher2                                                        //D
         {
             nextwave += waveskip;
         }
-        
         trv.Property("NextWave").SetValue(nextwave); //this.NextWave = num;
         trv.Method("StartWave").GetValue(); //this.StartWave();
-
-        //Plugin.Logger.LogInfo($"Nextwave {trv.Property("NextWave").GetValue<int>()}");
         return false;
     }
 }
@@ -142,12 +149,14 @@ class InfiniteAmmoPatcher                                               //Just r
         if(Plugin.InfiniteAmmo.Value)
         {
             return false;
+        } else
+        {
+            return true; //Returning true, so harmony does nothing to the function.
         }
-        return true;
-        
     }
 }
-[HarmonyPatch(typeof(ZS_PlayerControl), "MovementSpeed", MethodType.Getter)]                //Speedhack mod
+
+[HarmonyPatch(typeof(ZS_PlayerControl), "MovementSpeed", MethodType.Getter)]        //Speedhack mod
 class PlayerSpeedPatcher
 {
     static bool Prepare()
@@ -177,22 +186,15 @@ class PatchNoBuildingBlocked
 [HarmonyPatch(typeof(ZS_Health), "OnTakeDamage")]                                               //Godmode patcher + Infinite damage
 class betterGodModePatcher                                                      //Doesnt use ingame godmode, just set attack damage to 0.
 {
-    static void Prefix(ref ZS_ConnectedPlayerObject player, ref float damage)
+    static void Prefix(ZS_ConnectedPlayerObject player, ref float damage, ZS_Vector3 direction, EDamageType damageType, GameObject ownerObject, ref OnHealthChangeInfo info)
     {
         if(player==null && Plugin.EnableGodmode.Value) //Not a damage by a player but TO a player
         {
             damage = 0f;
         }
     }
-    static void Postfix(ref bool __result)
-    {
-        //Plugin.Logger.LogInfo("Postfix infinitedamage patcher was here");
-        __result = true;   // Will use     Plugin.InfiniteDamage.Value
-    }
 }
 
-
-//[HarmonyPatch(typeof(ZS_Health), "OnTakeDamage")]
 [HarmonyPatch(typeof(ZS_WaveZombieDogWave), "Process")]                                         // Halloween Force Mod
 class HalloweenForcePatch
 {
@@ -216,14 +218,12 @@ class OnTakeDamageTracker                                               //On eac
 {
     static void Prefix(ZS_ConnectedPlayerObject player, ref float damage, ZS_Health __instance)
     {
+        
         if (player != null && Plugin.InfiniteDamage.Value)
         {
             var tvr = Traverse.Create(__instance);
             float health = tvr.Field("health").GetValue<float>();
-            //float armour = tvr.Field("armour").GetValue<float>();
             damage = health*2;
-            //Plugin.Logger.LogInfo($"[Prefix] player : {player}, damage : {damage}, health : {health}");
-
         }
         
     }
@@ -232,14 +232,67 @@ class OnTakeDamageTracker                                               //On eac
         if (player != null && Plugin.InfiniteDamage.Value)
         {
             var tvr = Traverse.Create(__instance);
-            //float armour = tvr.Field("armour").GetValue<float>();
             float health = tvr.Field("health").GetValue<float>();
-            if(health>0)
+            if(health>0f)
             {
-                Plugin.Logger.LogWarning($"[InfiniteDamage] The zombie wasnt one shot ! health={health}, damage={damage}, return={__result}");
+                Plugin.Logger.LogError($"[InfiniteDamage] The zombie wasnt one shot ! health={health}, damage={damage}, return={__result}");
             }
 
         }
 
     }
 }
+
+                                            //New Build mode forcer
+// -----------------------------------------------------------------------------------------------------------
+[HarmonyPatch(typeof(ZS_NewInputSystemRequesterBuilding), "UpdateRequester")]
+class OnEnterTracker
+{  
+    static void Prefix(ZS_NewInputSystemRequesterBuilding __instance)
+    {
+        var tvr = Traverse.Create(__instance);
+        tvr.Field("canEnterBuildMode").SetValue(true);
+    }
+
+}
+
+[HarmonyPatch(typeof(ZS_PlayerControl), "IsBuildPhase")]
+class IsBuildPhasePatcher
+{
+    static void Postfix(ref bool __result)
+    {
+        __result = true;
+    }
+}
+
+[HarmonyPatch(typeof(ZS_NewInputSystemRequesterBuilding), "EnterBuildMode", [])]
+class EnterBuildModepPtcher2
+{
+    static bool Prefix(ZS_NewInputSystemRequesterBuilding __instance)
+    {
+        var playerControl = Traverse.Create(__instance).Property("GetPlayerControl").GetValue();
+        if (playerControl != null)
+        {
+            ZS_IOnBuildModeEnteredHelper.Call((ZS_PlayerControl)playerControl, true);
+        }
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(ZS_BuildModeManagerUI), "EnableBuildManagerUI")]
+class BuildUiTracker
+{
+    static void Prefix(ZS_BuildModeManagerUI __instance, ref bool enable)
+    {
+        var tvr = Traverse.Create(__instance);
+        bool canenter = tvr.Field("canEnterBuildMode").GetValue<bool>();
+        enable = true;
+        tvr.Field("canEnterBuildMode").SetValue(true);
+    }
+}
+// -----------------------------------------------------------------------------------------------------------
+
+
+
+
+
